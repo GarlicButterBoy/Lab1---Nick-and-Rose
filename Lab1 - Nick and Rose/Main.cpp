694ec841de332529cb8c035a47b0158ab6ded112
Nick Sturch-Flint <nick.rulez@live.com>
2020-09-20 14:33:28 -0400

    Added GenerateClientID() and other functions

--- Lab1 - Nick and Rose/Main.cpp
+++ Lab1 - Nick and Rose/Main.cpp
@@ -1,9 +1,77 @@
 // Lab1 - Nick and Rose.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <ctime>
 #include <iostream>
+
+#include "WorkTicket.h"
+
+using namespace std;
+
+/// <summary>
+///Turns a Char array into a string
+/// </summary>
+/// <param name="a"></param>
+/// <param name="size"></param>
+/// <returns>A string</returns>
+string ArrayToString(char* a , int size )
+{
+	string temp;
+	for (int i = 0; i < size; i++)
+	{
+		temp += a[i];
+	}
+	return temp;
+}
+
+/// <summary>
+/// Generates the ticket number (arbitrarily between 100 and 199) for the new ticket
+/// </summary>
+/// <returns>An Int</returns>
+int GenerateTicketNumber()
+{
+	int randNum;
+	do
+	{
+		randNum = rand();
+	} while (100 < randNum || 199 > randNum); //do loop generates a number until it lands between 100 and 199 (pedantic)
+
+	return  randNum;
+}
+
+/// <summary>
+/// Generate the random string of characters for the Client Identification Number
+/// </summary>
+/// <returns>A String</returns>
+string GenerateClientID()
+{
+	srand(time(0)); //using a seed allows for "not-so-true-but-basically" random generation
+
+	//array of valid characters to choose from
+	char valid[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVabcdefghijklmnopqrtuvwxyz";
+	char rand_array[6];
+	string output;
+
+	for (int i = 0; i < 6; i++)
+	{
+		rand_array[i] = valid[rand() % 63];				//rand_array (our 6 char alphanumeric string)
+	}													//valid[rand() % 63] generates a random character from the list
+	output = ArrayToString(rand_array, 6);			//63 is the total length of the valid[] array.
+	return output;										//from: https://www.codespeedy.com/generate-random-alphanumeric-string-in-cpp/
+}
+
+//;WorkTicket ReadWorkTicketInfo()
+//{
+
+//}
 
 int main()
 {
+
+	cout << "STARTING\n";
+	int numTest;
+	string test = GenerateClientID();
+	numTest = GenerateTicketNumber();
+	cout << endl << test << endl << numTest;
 
 }
--- Lab1 - Nick and Rose/WorkTicket.cpp
+++ Lab1 - Nick and Rose/WorkTicket.cpp
@@ -8,11 +8,15 @@
 #include "WorkTicket.h"
 #include <iostream>
 
+#include "MyConsoleInput.h"
+
 
 WorkTicket::WorkTicket(int ticketNumber, string ticketDate, string ticketID, string ticketDescription)
 {
 	myTicketNumber = ticketNumber;
-	my
+	myDate = ticketDate;
+	myID = ticketID;
+	myDescription = ticketDescription;
 }
 
 WorkTicket::~WorkTicket()
@@ -62,7 +66,23 @@ void WorkTicket::SetTicketNumber(int ticketNumber)
 
 void WorkTicket::SetTicketDate(int day, int month, int year)
 {
+	int myDay, myMonth, myYear;
+	int dayInput, monthInput, yearInput;
 
+	//Asks the user for the day of the month, then assigns it to myDay
+	cout << "Day: ";
+	dayInput = ConsoleInput::ReadInteger(1, 31);
+	myDay = dayInput;
+
+	//Asks the user for the month of the year, then assigns it to myMonth
+	cout << endl << "Month: ";
+	monthInput = ConsoleInput::ReadInteger(1, 12);
+	myMonth = monthInput;
+
+	//Asks the user for the year, then assigns it to myYear
+	cout << endl << "Year: ";
+	yearInput = ConsoleInput::ReadInteger(2000, 2099);
+	myYear = yearInput;
 }
 
 void WorkTicket::SetID(string clientID)
@@ -72,10 +92,10 @@ void WorkTicket::SetID(string clientID)
 
 void WorkTicket::SetDescription(string description)
 {
-
+	getline(cin, description);
 }
 
-string WorkTicket::ToString()
-{
+//string WorkTicket::ToString()
+//{
 
-}
+//}
